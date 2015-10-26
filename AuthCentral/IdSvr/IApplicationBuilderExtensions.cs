5a772909186e569651d2f97640e5bb5438974f71
Jason Burbage <jburbage@fsw.com>
2015-10-26 10:11:14 -0600

    Remove Identity Manager

--- AuthCentral/IdSvr/IApplicationBuilderExtensions.cs
+++ AuthCentral/IdSvr/IApplicationBuilderExtensions.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Owin;
 using Microsoft.Framework.DependencyInjection;
-using IdentityManager.Configuration;
 
 namespace Microsoft.AspNet.Builder
 {
@@ -37,52 +36,5 @@ namespace Microsoft.AspNet.Builder
             });
         }
 
-        public static void UseIdentityManager(this IApplicationBuilder app, IdentityManagerOptions options)
-        {
-            app.UseOwin(addToPipeline =>
-            {
-                addToPipeline(next =>
-                {
-                    var builder = new Microsoft.Owin.Builder.AppBuilder();
-
-                    // This might never be used...?
-                    var provider = app.ApplicationServices.GetService<Microsoft.AspNet.DataProtection.IDataProtectionProvider>();
-                    builder.Properties["security.DataProtectionProvider"] = new DataProtectionProviderDelegate(purposes =>
-                    {
-                        var dataProtection = provider.CreateProtector(String.Join(",", purposes));
-                        return new DataProtectionTuple(dataProtection.Protect, dataProtection.Unprotect);
-                    });
-
-                    builder.UseIdentityManager(options);
-
-                    var appFunc = builder.Build(typeof(Func<IDictionary<string, object>, Task>)) as Func<IDictionary<string, object>, Task>;
-                    return appFunc;
-                });
-            });
-        }
-
-        //public static void UseOpenIdConnectAuthentication(this IApplicationBuilder app, Microsoft.Owin.Security.OpenIdConnect.OpenIdConnectAuthenticationOptions options)
-        //{
-        //    app.UseOwin(addToPipeline =>
-        //    {
-        //        addToPipeline(next =>
-        //        {
-        //            var builder = new Microsoft.Owin.Builder.AppBuilder();
-
-        //            // This might never be used...?
-        //            var provider = app.ApplicationServices.GetService<Microsoft.AspNet.DataProtection.IDataProtectionProvider>();
-        //            builder.Properties["security.DataProtectionProvider"] = new DataProtectionProviderDelegate(purposes =>
-        //            {
-        //                var dataProtection = provider.CreateProtector(String.Join(",", purposes));
-        //                return new DataProtectionTuple(dataProtection.Protect, dataProtection.Unprotect);
-        //            });
-
-        //            builder.UseOpenIdConnectAuthentication(options);
-
-        //            var appFunc = builder.Build(typeof(Func<IDictionary<string, object>, Task>)) as Func<IDictionary<string, object>, Task>;
-        //            return appFunc;
-        //        });
-        //    });
-        //}
     }
 }
--- AuthCentral/Startup.cs
+++ AuthCentral/Startup.cs
@@ -1,12 +1,6 @@
-﻿using Fsw.Enterprise.AuthCentral.IdMgr;
-using Fsw.Enterprise.AuthCentral.IdSvr;
-using IdentityManager.Configuration;
-using IdentityManager.Core.Logging;
-using IdentityManager.Extensions;
-using IdentityManager.Logging;
+﻿using Fsw.Enterprise.AuthCentral.IdSvr;
 using IdentityServer3.Core.Configuration;
-using Microsoft.AspNet.Authentication.Cookies;
-using Microsoft.AspNet.Authentication.OpenIdConnect;
+using IdentityServer3.Core.Logging;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Dnx.Runtime;
@@ -15,10 +9,6 @@ using Microsoft.Framework.DependencyInjection;
 using Owin;
 using Serilog;
 using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Fsw.Enterprise.AuthCentral
 {
@@ -48,78 +38,7 @@ namespace Fsw.Enterprise.AuthCentral
 
             app.UseIISPlatformHandler();
             app.UseDeveloperExceptionPage();
-            LogProvider.SetCurrentLogProvider(new TraceSourceLogProvider());
-            
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationScheme = "Cookies"
-            });
-
-            var oidcOptions = new OpenIdConnectOptions("Cookies")
-            {
-                DisplayName = null, // this will hide the openidconnect button on the login page
-                // AuthenticationType = "oidc",
-                Authority = new UriBuilder(config.Uri.Scheme, config.Uri.Host, config.Uri.Port, "ids").Uri.AbsoluteUri,
-                ClientId = "idmgr_client",
-                RedirectUri = config.Uri.IssuerUri,
-                ResponseType = "id_token",
-                UseTokenLifetime = false,
-                Events = new OpenIdConnectEvents()
-                {
-                    OnAuthenticationValidated = n =>
-                    {
-                        // n.AuthenticationTicket.Properties.
-                        return Task.FromResult(0);
-                    }
-                },
-
-                //Notifications = new Microsoft.Owin.Security.OpenIdConnect.OpenIdConnectAuthenticationNotifications
-                //{
-                //    SecurityTokenValidated = n =>
-                //    {
-                //        n.AuthenticationTicket.Identity.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
-                //        return Task.FromResult(0);
-                //    },
-                //    RedirectToIdentityProvider = async n =>
-                //    {
-                //        if (n.ProtocolMessage.RequestType == Microsoft.IdentityModel.Protocols.OpenIdConnectRequestType.LogoutRequest)
-                //        {
-                //            var result = await n.OwinContext.Authentication.AuthenticateAsync("Cookies");
-                //            if (result != null)
-                //            {
-                //                var id_token = result.Identity.Claims.GetValue("id_token");
-                //                if (id_token != null)
-                //                {
-                //                    n.ProtocolMessage.IdTokenHint = id_token;
-                //                    n.ProtocolMessage.PostLogoutRedirectUri = new UriBuilder(Program.EnvConfig.Uri.Scheme, Program.EnvConfig.Uri.Host, Program.EnvConfig.Uri.Port, "idm").Uri.AbsoluteUri;
-                //                }
-                //            }
-                //        }
-                //    }
-                //}
-            };
-
-            oidcOptions.Scope.Add("idmgr");
-            app.UseOpenIdConnectAuthentication(oidcOptions);
-            
-            app.Map("/idm", idm =>
-            {
-                LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
-
-                var factory = new IdentityManagerServiceFactory();
-                factory.Configure(app, config.DB.MembershipReboot);
-
-                idm.UseIdentityManager(new IdentityManagerOptions
-                {
-                    Factory = factory,
-                    SecurityConfiguration = new HostSecurityConfiguration
-                    {
-                        HostAuthenticationType = "Cookies",
-                    }
-
-                });
-            });
-
+                        
             app.Map("/ids", ids =>
             {
                 Log.Logger = new LoggerConfiguration()

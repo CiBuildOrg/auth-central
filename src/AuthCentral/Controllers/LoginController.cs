34089665f0a230d365539402071ec9126d12ffde
Malick McGregor <MMcGregor@fsw.com>
2015-10-29 09:24:10 -0600

    Added MVC components for the new login response page.

---
+++ src/AuthCentral/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using IdentityServer3.Core.ViewModels;
+using Microsoft.AspNet.Mvc;
+
+namespace Fsw.Enterprise.AuthCentral.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(LoginCredentials model)
+        {
+            return View();
+        }
+    }
+}

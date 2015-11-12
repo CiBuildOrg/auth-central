c39f66e22f6bd58c4003ee3ada6b819bda6fc9f2
Malick McGregor <MMcGregor@fsw.com>
2015-11-12 10:23:08 -0700

    Fix DI registration of MembershipRebootSetup so that it is used as a MemebrshipRebootConfiguration. Also registered HierarchicalUserAccount as UserAccount. May not be necessary, but shouldn't hurt.

--- src/AuthCentral/IdSvr/UserServiceExtensions.cs
+++ src/AuthCentral/IdSvr/UserServiceExtensions.cs
@@ -12,9 +12,10 @@ namespace Fsw.Enterprise.AuthCentral.IdSvr
     {
         public static void ConfigureCustomUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app, string connString)
         {
+            factory.Register(new Registration<UserAccount, HierarchicalUserAccount>());
+            factory.Register(new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(x => MembershipRebootSetup.GetConfig(app)));
+            factory.Register(new Registration<IUserAccountRepository<HierarchicalUserAccount>, MongoUserAccountRepository<HierarchicalUserAccount>>());
             factory.Register(new Registration<UserAccountService<HierarchicalUserAccount>>());
-            factory.Register(new Registration<MembershipRebootSetup>(MembershipRebootSetup.GetConfig(app)));
-            factory.Register(new Registration<IUserAccountRepository<HierarchicalUserAccount>,  MongoUserAccountRepository<HierarchicalUserAccount>>());
             factory.Register(new Registration<MongoDatabase>(resolver => new MongoDatabase(connString)));
         }
     }    

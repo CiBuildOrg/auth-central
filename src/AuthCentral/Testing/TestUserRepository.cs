f345e7364e8ea706a584a61f3d56147ef6a7fef8
Malick McGregor <mmcgregor@fsw.com>
2015-11-17 14:20:40 -0700

    Created implementation of User Repository.

---
+++ src/AuthCentral/Testing/TestUserRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+
+namespace Fsw.Enterprise.AuthCentral.Testing
+{
+    public class TestUserRepository : IUserAccountRepository<HierarchicalUserAccount>
+    {
+        private Dictionary<Guid, HierarchicalUserAccount> _accounts;
+
+        private Dictionary<Guid, HierarchicalUserAccount> Accounts
+            => _accounts ?? (_accounts = new Dictionary<Guid, HierarchicalUserAccount>());
+
+        public HierarchicalUserAccount Create()
+        {
+            var account = new HierarchicalUserAccount();
+            Accounts.Add(account.ID, account);
+
+            return account;
+        }
+
+        public void Add(HierarchicalUserAccount item)
+        {
+            if(Accounts.ContainsKey(item.ID))
+                return;
+
+            Accounts.Add(item.ID, item);
+        }
+
+        public void Remove(HierarchicalUserAccount item)
+        {
+            if (Accounts.ContainsKey(item.ID))
+                Accounts.Remove(item.ID);
+        }
+
+        public void Update(HierarchicalUserAccount item)
+        {
+            if (Accounts.ContainsKey(item.ID))
+                Accounts[item.ID] = item;
+        }
+
+        public HierarchicalUserAccount GetByUsername(string username)
+        {
+            return Accounts.Values.SingleOrDefault(a => a.Username == username);
+        }
+
+        public HierarchicalUserAccount GetByUsername(string tenant, string username)
+        {
+            return Accounts.Values.SingleOrDefault(a => a.Tenant == tenant && a.Username == username);
+        }
+
+        public HierarchicalUserAccount GetByEmail(string tenant, string email)
+        {
+            return Accounts.Values.SingleOrDefault(a => a.Email == email && a.Tenant == tenant);
+        }
+
+        public HierarchicalUserAccount GetByMobilePhone(string tenant, string phone)
+        {
+            return Accounts.Values.SingleOrDefault(a => a.Tenant == tenant && a.MobilePhoneNumber == phone);
+        }
+
+        public HierarchicalUserAccount GetByVerificationKey(string key)
+        {
+            return Accounts.Values.SingleOrDefault(a => a.VerificationKey == key);
+        }
+
+        public HierarchicalUserAccount GetByLinkedAccount(string tenant, string provider, string id)
+        {
+            return
+                Accounts.Values.SingleOrDefault(
+                    a =>
+                        a.Tenant == tenant &&
+                        a.LinkedAccounts.Any(
+                            account => account.ProviderAccountID == id && account.ProviderName == provider));
+        }
+
+        public HierarchicalUserAccount GetByCertificate(string tenant, string thumbprint)
+        {
+            return
+                Accounts.Values.SingleOrDefault(
+                    account =>
+                        account.Tenant == tenant &&
+                        account.Certificates.Any(certificate => certificate.Thumbprint == thumbprint));
+        }
+
+        public HierarchicalUserAccount GetByID(Guid id)
+        {
+            return !Accounts.ContainsKey(id) ? null : Accounts[id];
+        }
+    }
+}

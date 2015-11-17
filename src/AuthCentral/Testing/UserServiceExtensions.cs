9b9cdea62c24602a30ee44242e8baf14eabdb550
Malick McGregor <mmcgregor@fsw.com>
2015-11-17 14:21:04 -0700

    Made a class to register dependencies for testing data.

---
+++ src/AuthCentral/Testing/UserServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+using Fsw.Enterprise.AuthCentral.IdMgr;
+using IdentityServer3.Core.Configuration;
+using Microsoft.AspNet.Builder;
+
+namespace Fsw.Enterprise.AuthCentral.Testing
+{
+    public static class UserServiceExtensions
+    {
+        public static void ConfigureTestUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app, string connString)
+        {
+            factory.Register(new Registration<UserAccount, HierarchicalUserAccount>());
+            factory.Register(new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(x => MembershipRebootSetup.GetConfig(app)));
+            factory.Register(new Registration<IUserAccountRepository<HierarchicalUserAccount>, TestUserRepository>());
+            factory.Register(new Registration<UserAccountService<HierarchicalUserAccount>>());
+        }
+    }
+}

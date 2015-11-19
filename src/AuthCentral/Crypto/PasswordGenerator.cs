f3986db2badcb9c369f2d9435f4313d5fef537c8
Jason Burbage <jburbage@fsw.com>
2015-11-19 11:06:35 -0700

    Changing password generator to internal

--- src/AuthCentral/Crypto/PasswordGenerator.cs
+++ src/AuthCentral/Crypto/PasswordGenerator.cs
@@ -3,10 +3,10 @@ using System.Text;
 
 namespace Fsw.Enterprise.AuthCentral.Crypto
 {
-    public class PasswordGenerator
+    class PasswordGenerator
     {
         // http://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
-        public static string GeneratePasswordOfLength(int length)
+        internal static string GeneratePasswordOfLength(int length)
         {
             char[] chars = new char[62];
             chars =

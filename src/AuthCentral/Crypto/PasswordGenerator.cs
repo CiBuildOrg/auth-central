a30b9f254ac3e1303671b4a3c757f66d65abafd2
Jason Burbage <jburbage@fsw.com>
2015-11-19 15:15:46 -0700

    Clean up password generator

--- src/AuthCentral/Crypto/PasswordGenerator.cs
+++ src/AuthCentral/Crypto/PasswordGenerator.cs
@@ -8,14 +8,11 @@ namespace Fsw.Enterprise.AuthCentral.Crypto
         // http://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
         internal static string GeneratePasswordOfLength(int length)
         {
-            char[] chars = new char[62];
-            chars =
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
-            byte[] data = new byte[1];
+            byte[] data = new byte[length];
+            char[] chars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]\{}|;':./,<>?".ToCharArray();
+
             using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
             {
-                crypto.GetNonZeroBytes(data);
-                data = new byte[length];
                 crypto.GetNonZeroBytes(data);
             }
             StringBuilder result = new StringBuilder(length);

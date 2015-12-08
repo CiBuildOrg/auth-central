ead2efea55feb10b650e449380e122b84033c9c3
Malick McGregor <MMcGregor@fsw.com>
2015-12-08 12:29:22 -0700

    Added testing IScopeService.

---
+++ src/AuthCentral/Testing/MemoryScopeService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fsw.Enterprise.AuthCentral.MongoStore.Admin;
+using IdentityServer3.Core.Models;
+
+namespace Fsw.Enterprise.AuthCentral.Testing
+{
+    public class MemoryScopeService : IScopeService
+    {
+        private static readonly Dictionary<string, Scope> Scopes = Testing.Scopes.Get()
+            .ToDictionary(scope => scope.Name);
+
+        public Task Save(Scope scope)
+        {
+            if (Scopes.ContainsKey(scope.Name))
+                Scopes[scope.Name] = scope;
+            else
+            {
+                Scopes.Add(scope.Name, scope);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public Task Delete(string scopeName)
+        {
+            Scopes.Remove(scopeName);
+
+            return Task.FromResult(0);
+        }
+
+        public Task<Scope> Find(string scopeName)
+        {
+            return Scopes.ContainsKey(scopeName) ? Task.FromResult(Scopes[scopeName]) : null;
+        }
+
+        public Task<IEnumerable<Scope>> Find(IEnumerable<string> scopeNames)
+        {
+            return Task.FromResult(Scopes.Where(pair => scopeNames.Contains(pair.Key)).Select(pair => pair.Value));
+        }
+    }
+}

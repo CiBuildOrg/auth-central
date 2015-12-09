96bda48bfa3b348e1e2dc14d13c5c81459c34483
Jordon Storfa <looking.promising@gmail.com>
2015-12-09 13:49:08 -0700

    switch to dnx452

--- src/AuthCentral/Extensions/IApplicationBuilderExtensions.cs
+++ src/AuthCentral/Extensions/IApplicationBuilderExtensions.cs
@@ -45,7 +45,7 @@ namespace Fsw.Enterprise.AuthCentral.Extensions
 
             var options = new IdentityServerOptions
             {
-                SiteName = "FSW Identity Server",
+                SiteName = "FSW",
                 PublicOrigin = config.Uri.IssuerUri,
                 SigningCertificate = Certificate.Get(config.Cert.StoreName, config.Cert.Thumbprint),
                 IssuerUri = config.Uri.IssuerUri,

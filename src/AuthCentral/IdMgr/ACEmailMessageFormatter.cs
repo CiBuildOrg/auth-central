888b66651ea8fdb10887287894029bb315437b89
Malick McGregor <MMcGregor@fsw.com>
2015-12-11 14:05:06 -0700

    Initial add of an AuthCentral email message formatter. Empty.

---
+++ src/AuthCentral/IdMgr/ACEmailMessageFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+
+namespace Fsw.Enterprise.AuthCentral.IdMgr
+{
+    internal class ACEmailMessageFormatter : EmailMessageFormatter<HierarchicalUserAccount>
+    {
+        public ACEmailMessageFormatter(AuthCentralAppInfo appInfo) : base(appInfo)
+        {
+        }
+
+        public ACEmailMessageFormatter(Lazy<ApplicationInformation> appInfo) : base(appInfo)
+        {
+        }
+
+
+    }
+}

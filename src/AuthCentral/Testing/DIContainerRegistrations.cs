9a9f3bda74e259598ec516769c179008a4768762
Malick McGregor <MMcGregor@fsw.com>
2015-12-14 15:39:11 -0700

    Added ApplicationEnvironment calls to test MRSetup calls.

--- src/AuthCentral/Testing/DIContainerRegistrations.cs
+++ src/AuthCentral/Testing/DIContainerRegistrations.cs
@@ -4,22 +4,23 @@ using Fsw.Enterprise.AuthCentral.IdMgr;
 using IdentityServer3.Core.Configuration;
 using Microsoft.AspNet.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Fsw.Enterprise.AuthCentral.Testing
 {
     public static class DIContainerRegistrations
     {
-        public static void ConfigureTestUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app)
+        public static void ConfigureTestUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app, IApplicationEnvironment appEnvironment)
         {
             factory.Register(new Registration<UserAccount, HierarchicalUserAccount>());
-            factory.Register(new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(x => MembershipRebootSetup.GetConfig(app)));
+            factory.Register(new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(x => MembershipRebootSetup.GetConfig(app, appEnvironment)));
             factory.Register(new Registration<IUserAccountRepository<HierarchicalUserAccount>, TestUserRepository>());
             factory.Register(new Registration<UserAccountService<HierarchicalUserAccount>>());
         }
 
         public static void ConfigureIServiceCollection(IServiceCollection services)
         {
-            services.AddScoped<MembershipRebootConfiguration<HierarchicalUserAccount>>(provider => MembershipRebootSetup.GetConfig(null));
+            services.AddScoped<MembershipRebootConfiguration<HierarchicalUserAccount>>(provider => MembershipRebootSetup.GetConfig(null, provider.GetService<IApplicationEnvironment>()));
             services.AddScoped<UserAccountService<HierarchicalUserAccount>>();
             services.AddScoped(typeof(IUserAccountRepository<HierarchicalUserAccount>), typeof(TestUserRepository));
             services.AddScoped<MongoAuthenticationService>();

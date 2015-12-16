f1a83209b5c0b4ea93646a8639adf5277ecd9b96
Jason Burbage <jburbage@fsw.com>
2015-12-16 11:10:33 -0700

    Finish profile pages, including test cases

---
+++ src/AuthCentral/Areas/Admin/Models/UserProfileModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fsw.Enterprise.AuthCentral.Areas.Admin.Models
+{
+    public class UserProfileModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        /// <summary>
+        /// User's given (first) name.
+        /// </summary>
+        [Required]
+        public string GivenName { get; set; }
+
+        /// <summary>
+        /// User's middle name.
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// User's family (last) name.
+        /// </summary>
+        [Required]
+        public string FamilyName { get; set; }
+
+    }
+}

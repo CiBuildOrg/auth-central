00649322f0c8196b7f9b6e6f98070753e713b426
Malick McGregor <MMcGregor@fsw.com>
2016-01-04 16:21:59 -0700

    Added RemoveClaim button and associated callback.

--- src/AuthCentral/Areas/Admin/Controllers/ScopeController.cs
+++ src/AuthCentral/Areas/Admin/Controllers/ScopeController.cs
@@ -1,5 +1,8 @@
-﻿using Fsw.Enterprise.AuthCentral.Areas.Admin.Models;
+﻿using System.Linq;us
+using System.Threading.Tasks;
+using Fsw.Enterprise.AuthCentral.Areas.Admin.Models;
 using Fsw.Enterprise.AuthCentral.MongoStore.Admin;
+using IdentityServer3.Core.Models;
 using Microsoft.AspNet.Mvc;
 
 namespace Fsw.Enterprise.AuthCentral.Areas.Admin.Controllers
@@ -24,6 +27,37 @@ namespace Fsw.Enterprise.AuthCentral.Areas.Admin.Controllers
         [HttpPost("[action]")]
         public IActionResult Edit()
         {
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Removes the given claim from the given scope.
+        /// </summary>
+        /// <param name="scope">Unique identifier for the scope.</param>
+        /// <param name="claim">Unique identifier for the claim.</param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RemoveClaim(string scope, string claim)
+        {
+            var editScope = await _scopeService.Find(scope);
+
+            if (editScope == null)
+            {
+                ModelState.AddModelError("FindScope",$"Scope with the name {scope} could not be found.");
+                return RedirectToAction("Index");
+            }
+
+            var claimToRemove = editScope.Claims.SingleOrDefault(scopeClaim => scopeClaim.Name == claim);
+
+            if (claimToRemove == default(ScopeClaim))
+            {
+                ModelState.AddModelError("FindClaim", $"Claim with the name {claim} was not found in the scope {scope}");
+                return RedirectToAction("Index");
+            }
+
+            editScope.Claims.Remove(claimToRemove);
+            await _scopeService.Save(editScope);
+
             return RedirectToAction("Index");
         }
     }

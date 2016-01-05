41a674962db6e5a73ef607ceb7707999714da30e
Jordon Storfa <looking.promising@gmail.com>
2016-01-04 22:45:32 -0700

    build styles using bower, grunt and sass

--- src/AuthCentral/gulpfile.js
+++ src/AuthCentral/gulpfile.js
@@ -2,63 +2,85 @@
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
-
 var gulp = require('gulp');
-var rimraf = require('rimraf');
 var concat = require('gulp-concat');
+
+var fs = require('fs');
+var merge2 = require('merge2');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var rev = require('gulp-rev');
+var replace = require('gulp-html-replace');
+var bower = require('gulp-bower');
 
 var paths = {
     bower: "./bower_components/",
-    lib: "./wwwroot/libs/"
+    lib: "./wwwroot/libs/",
+		assets: "./wwwroot/assets/"
 };
 
-gulp.task('clean', function (callback) {
-    rimraf(paths.lib, callback);
+gulp.task('bower', function () {
+    return bower()
+      .pipe(gulp.dest(config.bowerDir)) });
+
+gulp.task('icons', function() {     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')       .pipe(gulp.dest(paths.assets + 'fonts')); 
 });
 
-gulp.task('default', ['clean'], function () {
-    var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
-        "jquery": "jquery/jquery*.{js,map}",
-        "jquery-validation": "jquery-validation/jquery.validate*.js",
-        "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive*.js"
-    };
+gulp.task('default', ['watch']);
 
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir]).pipe(gulp.dest(paths.lib + destinationDir));
-    }
+gulp.task('build', ['sass'], function() {
+	return gulp.start('layout');
 });
 
-gulp.task( 'concat' , function () {
-    gulp.src([
-        paths.lib + 'bootstrap/js/bootstrap.min.js',
-        paths.lib + 'jquery/jquery.min.js',
-        paths.lib + 'jquery-validation/jquery.validate.min.js',
-        paths.lib + 'jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js'
-    ])
-        .pipe(concat( "bundle.js"))
-        .pipe(gulp.dest(paths.lib));
+gulp.task('build:sass', ['sass'], function() {
+	return gulp.start('layout');
 });
 
+gulp.task('clean:sass', function() {
+  del(paths.assets + '**/*.css');
+  return gulp.start('bower');
+});
 
+gulp.task('watch', ['watch:sass']);
 
+gulp.task('watch:sass', function () {
+	return gulp.watch('./Styles/**/*.scss', function() {
+		return gulp.start('build:sass');
+	});
+});
 
+gulp.task('sass', ['clean:sass'], function () {
+    return gulp.src(['./Styles/main.scss', './Styles/vendor.scss'])
+		.pipe(sass({
+            loadPath: [
+                './Styles',
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                config.bowerDir + '/fontawesome/scss',
+            ]
+        })
+     )
+		.pipe(rev())
+		.pipe(gulp.dest(paths.assets) + 'css');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gulp.task('layout', function() {
+	var files = fs.readdirSync(paths.assets).reverse().map(function(f) { return '/assets/' + f; });
+	
+	var cssFiles = files.filter(function(f) {
+		return f.indexOf('.css') !== -1;
+	});
+	
+	var jsFiles = files.filter(function(f) {
+		return f.indexOf('.js') !== -1;
+	});
+	
+	return gulp.src('./Areas/UserAccount/Views/Shared/_Layout.cshtml')
+		.pipe(replace({
+			'css': cssFiles,
+			'js': jsFiles
+		}, { 
+			keepBlockTags: true 
+		}))
+		.pipe(gulp.dest('./Areas/UserAccount/Views/Shared/'));
+});

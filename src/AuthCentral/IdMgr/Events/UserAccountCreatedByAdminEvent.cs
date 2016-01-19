8d038baa887970e3a097ff8f4bc1f67b476255f8
Jason Burbage <jburbage@fsw.com>
2016-01-18 17:30:45 -0700

    Extend UserAccountService for admin area

---
+++ src/AuthCentral/IdMgr/Events/UserAccountCreatedByAdminEvent.cs
@@ -0,0 +1,13 @@
+﻿using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fsw.Enterprise.AuthCentral.IdMgr.Events
+{
+    public class UserAccountCreatedByAdminEvent<TAccount> : UserAccountEvent<TAccount> where TAccount : UserAccount {
+        public string VerificationKey { get; set; }
+    }
+}

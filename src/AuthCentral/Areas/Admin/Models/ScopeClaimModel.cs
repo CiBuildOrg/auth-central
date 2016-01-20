6b3577f96b27f239d2a905f683052509a418bbd4
Malick McGregor <MMcGregor@fsw.com>
2016-01-20 15:08:47 -0700

    Added scope safe name to ScopeClaimModel and usage to ScopeClaimRow

--- src/AuthCentral/Areas/Admin/Models/ScopeClaimModel.cs
+++ src/AuthCentral/Areas/Admin/Models/ScopeClaimModel.cs
@@ -44,6 +44,11 @@ namespace Fsw.Enterprise.AuthCentral.Areas.Admin.Models
         internal ScopeClaim ScopeClaim { get; }
 
         /// <summary>
+        /// Name of the scope so that it can be used in javascript.
+        /// </summary>
+        public string SafeScopeName => ScopeName.Replace(':', '_');
+
+        /// <summary>
         /// Name of the claim so that it can be used in javascript.
         /// </summary>
         public string SafeName => Name.Replace(':', '_');

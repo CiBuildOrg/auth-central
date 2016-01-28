e9a416770c770a3cda772e2399d9824a84ad00cc
TRendall <trendall@fsw.com>
2016-01-28 16:49:08 -0700

    removing env variable so that we can pass the url in the script.
    added MasterSuite to run all the tests.

--- test/ide-ui-integration-tests/runtest.bat
+++ test/ide-ui-integration-tests/runtest.bat
@@ -10,4 +10,4 @@ IF /I %1 == stage (
 
 echo Root URL set to: %URL%
 
-java.exe -jar C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\selenium-server-standalone-2.48.2.jar -htmlSuite "*firefox" "%URL%" "C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\Architecture_UITests.html" "C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\Logs\IdeTestResults.html" -port 5555
+java.exe -jar C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\selenium-server-standalone-2.48.2.jar -htmlSuite "*firefox" "%URL%" "C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\MasterSuite.html" "C:\Jenkins\workspace\auth_central_deploy_smoke_tests\test\ide-ui-integration-tests\Logs\IdeTestResults.html" -port 5555

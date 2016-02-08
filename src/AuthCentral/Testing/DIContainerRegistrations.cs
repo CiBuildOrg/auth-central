530c0417d8d737956d475d5df40c99de486e7abf
Malick McGregor <MMcGregor@fsw.com>
2016-02-08 14:07:03 -0700

    Fixed dependency construction for MembershipRebootConfiguration in ConfigureTestUserService.

--- src/AuthCentral/Testing/DIContainerRegistrations.cs
+++ src/AuthCentral/Testing/DIContainerRegistrations.cs
@@ -4,16 +4,20 @@ using Fsw.Enterprise.AuthCentral.IdMgr;
 using IdentityServer3.Core.Configuration;
 using Microsoft.AspNet.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Fsw.Enterprise.AuthCentral.Testing
 {
     internal static class DiContainerRegistrations
     {
-        public static void ConfigureTestUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app, IApplicationEnvironment appEnvironment, EnvConfig config)
+        public static void ConfigureTestUserService(this IdentityServerServiceFactory factory, IApplicationBuilder app, IApplicationEnvironment appEnvironment,
+            EnvConfig config)
         {
             factory.Register(new Registration<UserAccount, HierarchicalUserAccount>());
-            factory.Register(new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(x => MembershipRebootConfigFactory.GetDefaultConfig(appEnvironment, config)));
+            factory.Register(
+                new Registration<MembershipRebootConfiguration<HierarchicalUserAccount>>(
+                    x => MembershipRebootConfigFactory.GetDefaultConfig(appEnvironment, x.Resolve<ILoggerFactory>(), config)));
             factory.Register(new Registration<IUserAccountRepository<HierarchicalUserAccount>, TestUserRepository>());
             factory.Register(new Registration<IBulkUserRepository<HierarchicalUserAccount>, TestBulkUserRepository>());
             factory.Register(new Registration<UserAccountService<HierarchicalUserAccount>>());
@@ -21,9 +25,10 @@ namespace Fsw.Enterprise.AuthCentral.Testing
 
         public static void ConfigureIServiceCollection(IServiceCollection services, IApplicationEnvironment env, EnvConfig config)
         {
-            services.AddScoped<MembershipRebootConfiguration<HierarchicalUserAccount>>(provider => MembershipRebootConfigFactory.GetDefaultConfig(env, config));
+            services.AddScoped<MembershipRebootConfiguration<HierarchicalUserAccount>>(
+                provider => MembershipRebootConfigFactory.GetDefaultConfig(env, provider.GetService<ILoggerFactory>(), config));
             services.AddScoped<UserAccountService<HierarchicalUserAccount>>();
-            services.AddScoped(typeof(IUserAccountRepository<HierarchicalUserAccount>), typeof(TestUserRepository));
+            services.AddScoped(typeof (IUserAccountRepository<HierarchicalUserAccount>), typeof (TestUserRepository));
             services.AddScoped<IBulkUserRepository<HierarchicalUserAccount>, TestBulkUserRepository>();
             services.AddScoped<MongoAuthenticationService>();
         }
